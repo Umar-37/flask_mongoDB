fc390bf12538d37cd4b454838abe77c2bfa8b000
Omar Alhisan <ohisan@wirefilter.com>
2024-02-08 11:10:31 +0300

    Template use g.user

--- blog/auth.py
+++ blog/auth.py
@@ -26,9 +26,7 @@ def login_required(view):
     def wrapped_view(**kwargs):
         if g.user is None:
             return redirect(url_for('auth.login'))
-
         return view(**kwargs)
-
     return wrapped_view
 
 @bp.route('/login', methods=('GET', 'POST'))
