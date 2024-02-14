1f3dae7db561e012d5e2f563e6d63b4fc9d68e94
Omar Alhisan <ohisan@wirefilter.com>
2024-02-14 12:18:08 +0300

    Add pagination

--- blog/posts.py
+++ blog/posts.py
@@ -1,5 +1,5 @@
 from bson.objectid import ObjectId
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from .auth import login_required
 from .db import get_database
@@ -12,8 +12,18 @@ posts = dbname["posts"]
 
 @bp.route("/")
 def list():
-    rows = posts.find().sort({'date':-1})
-    return render_template('posts/list.html', posts=rows)
+    page_size = 10
+    current_page = int(request.args.get('page', 1))
+    skip = (current_page - 1) * page_size
+    pipeline = [
+        {'$sort': {'date': -1}},
+        {'$skip': skip},
+        {'$limit': page_size}
+    ]
+    rows = posts.aggregate(pipeline)
+    total_posts = posts.count_documents({})
+    pages = (total_posts + page_size - 1) // page_size
+    return render_template('posts/list.html', posts=rows, pages=pages, current_page=current_page)
 
 @bp.route("/<id>")
 def view(id):
